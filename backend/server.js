bc070264e5d6d1c7fee2b77e96b21aa1b66624cd
ooje2022 <johnson.o.ojeniyi@gmail.com>
2023-06-23 19:52:15 +0100

    extend CORS access to render.com

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "mern-taskmgr-app.onrender.com"],
   })
 );
 app.use("/api/tasks", taskRoutes);
